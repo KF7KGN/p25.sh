2c75012e4a2ee099a08dfc6c3556bffaa615a2d5
KF7KGN <149395979+KF7KGN@users.noreply.github.com>
2024-04-14 12:27:19 -0600

    Add files via upload

---
+++ p25.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# echo "starting screen" \ &
+
+# sudo screen \ &
+
+echo "Starting P25 to DMR Bridge" \ &
+
+echo "Opening /usr/local/bin and starting P25 Reflector and Gateway." \ &
+
+cd /usr/local/bin/ && ./P25Reflector.sh start /etc/P25Gateway.ini \ &
+
+echo "Done" \ &
+
+echo "Opening /etc and /Opt P25Gateway.ini" / &
+
+cd /opt/P25Gateway && ./P25Gateway /etc/P25Gateway.ini \ &
+
+echo "Done" \ &
+
+echo "Starting Analog_Bridge_DMR and .ini files" \ &
+
+cd /opt/Analog_Bridge && ./Analog_Bridge_DMR /etc/Analog_Bridge_DMR.ini \ &
+
+echo "Done" \ &
+
+echo "Starting /opt Analog_Bridge_P25" \ &
+
+cd /opt/Analog_Bridge && ./Analog_Bridge_P25 /etc/Analog_Bridge_P25.ini \ &
+
+echo "Done" \ &
+
+echo "Stopping MMDVM_Bridge services to confirm the ports are closed!!" \ &
+
+sudo systemctl stop mmdvm_bridge.service / &
+
+echo "Starting MMDVM_Bridge services now...." / &
+
+cd /opt/MMDVM_Bridge && ./MMDVM_Bridge /etc/MMDVM_Bridge.ini / &
+
+echo "Started"
